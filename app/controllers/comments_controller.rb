237d0cd60a30ab547446a8b1ffe174c4db08b59e
anna-dorigo <alejandr@gmail.com>
2019-09-16 11:44:37 -0400

    add comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+	def create 
+		@articulo = Article.find(params[:article_id])
+		@comment = @articulo.comments.create(comment_params)
+		redirect_to article_path(@articulo)
+	end
+
+	private
+		def comment_params
+			params.require(:comment).permit(:commenter, :body)
+		end
+	
 end
