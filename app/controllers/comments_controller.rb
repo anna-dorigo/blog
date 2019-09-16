131c47ff0dc98b8672111b0ded58889ef15d115c
anna-dorigo <alejandr@gmail.com>
2019-09-16 12:09:32 -0400

    delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
 		redirect_to article_path(@articulo)
 	end
 
+	def destroy
+		@articulo = Article.find(params[:article_id])
+		@comment = @articulo.comments.find(params[:id])
+		@comment.destroy
+		redirect_to article_path(@articulo)
+	end
+
 	private
 		def comment_params
 			params.require(:comment).permit(:commenter, :body)
